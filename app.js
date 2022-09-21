bf466fdc09b9e0b7c43e1249980e09fdfda13eba
Arrow93 <113959108+Arrow93@users.noreply.github.com>
2022-09-21 14:31:07 +0630

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const { response } = require("express");
-const express = require("express");
-const app = express();
-
-app.get("/",function(req, resp){
-    resp.sendFile(__dirname + "/wys.html");
-});
-app.listen(3000);
